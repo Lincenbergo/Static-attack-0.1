3bb62aba1f96e8a662429a72fb227508399b89e5
Lincenbergo <48567874+Lincenbergo@users.noreply.github.com>
2019-05-02 16:05:08 -0300

    Add files via upload

---
+++ Control.gd
@@ -0,0 +1,7 @@
+extends Control
+
+
+
+func _on_Nueva_Partida_pressed():
+	get_tree().change_scene("res://Escenas/Niveles/Nivel 1/nivel 1.tscn")
+
---
+++ ControlT.gd
@@ -0,0 +1,5 @@
+extends Control
+func _ready():
+	pass # Replace with function body.
+func _on_IZ_pressed():
+	pass # Replace with function body.
